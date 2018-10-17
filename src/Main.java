a2b8ab5e92cb30116f30a9c95c0eccd546ee9176
cherryBlossomss <2712827632@qq.com>
2018-10-17 15:20:55 +0800

    my second commit

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.printf("第一个GitHub项目");
         System.out.println("我修改了哦");
+        System.out.printf("我不满意");
     }
-
-
 }
